97b5c77436b4400312f1de041b8d21ccb73b9188
Logan <logansrooks@gmail.com>
2017-12-01 02:20:04 -0500

    editing UltrasonicController interface

--- src/UltrasonicController.java
+++ src/UltrasonicController.java
@@ -1,9 +1,9 @@
 
-import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.NXTRegulatedMotor;
 import lejos.hardware.motor.Motor;
 public interface UltrasonicController {
 	static final int DEFAULT_SPEED = 200;
-	static final EV3LargeRegulatedMotor leftMotor = Motor.A, rightMotor = Motor.C;
+	static final NXTRegulatedMotor leftMotor = Motor.B, rightMotor = Motor.C;
 	
 	public int readSensorDistance();
 	
